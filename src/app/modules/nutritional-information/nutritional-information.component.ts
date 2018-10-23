012e957bb80caa71d602c357ce45e955ac0eb300
josueben <josuebenbeatle@outlook.com>
2018-10-22 23:34:23 -0500

    Listo

--- src/app/modules/nutritional-information/nutritional-information.component.ts
+++ src/app/modules/nutritional-information/nutritional-information.component.ts
@@ -10,34 +10,42 @@ import { FoodService } from '../../services/food.service';
 })
 export class NutritionalInformationComponent implements OnInit {
 
-  public FoodInfo: Food;
-  public AllFoods: Food[];
+  public FoodInfo: Food; // En este se carga el item food que se mostrara
+  public AllFoods: Food[] = [];
   public UserFoodsAnwer: FoodPreferenceAnswer[];
-  public UserFoods: Food[];
+  public UserFoods: Food[] = []; // Son los foods del usuario
 
   constructor(
     public foodService: FoodService
   ) {
     this.foodService.getAllFoods().subscribe((response: Food[]) => {
       this.AllFoods = response;
-      this.foodService.getAllFoods().subscribe((responseUserFoods: FoodPreferenceAnswer[]) => {
+      console.log(this.AllFoods);
+      this.foodService.getUserFoods().subscribe((responseUserFoods: FoodPreferenceAnswer[]) => {
         this.UserFoodsAnwer = responseUserFoods;
-        for (let item = 0; item < this.UserFoods.length; item++) {
-          if (this.AllFoods[item].id === this.UserFoodsAnwer[item].id) {
-            this.UserFoods.push(new Food(
-              this.AllFoods[item].id,
-              this.AllFoods[item].name,
-              this.AllFoods[item].kind,
-              this.AllFoods[item].portion,
-              this.AllFoods[item].unity,
-              this.AllFoods[item].net_weight,
-              this.AllFoods[item].calories,
-              this.AllFoods[item].protein,
-              this.AllFoods[item].lipids,
-              this.AllFoods[item].cho
-            ));
+        console.log(this.UserFoodsAnwer);
+        let foundedFood = 0;
+        console.log(this.UserFoodsAnwer.length);
+          for (let item = 0; item < this.AllFoods.length; item++) {
+            for (let itemUser = 0; itemUser < this.UserFoodsAnwer.length; itemUser++) {
+              if (this.AllFoods[item].id === this.UserFoodsAnwer[itemUser].id) {
+                console.log('Holi');
+                this.UserFoods.push(new Food(
+                  this.AllFoods[item].id,
+                  this.AllFoods[item].name,
+                  this.AllFoods[item].kind,
+                  this.AllFoods[item].portion,
+                  this.AllFoods[item].unity,
+                  this.AllFoods[item].net_weight,
+                  this.AllFoods[item].calories,
+                  this.AllFoods[item].protein,
+                  this.AllFoods[item].lipids,
+                  this.AllFoods[item].cho
+                ));
+                foundedFood++;
+              }
+            }
           }
-        }
       });
     });
   }
