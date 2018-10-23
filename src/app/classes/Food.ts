2a56c72f77cdd13a4c74f322fa581dbd85374121
josueben <josuebenbeatle@outlook.com>
2018-10-22 23:53:15 -0500

    Lito

--- src/app/classes/Food.ts
+++ src/app/classes/Food.ts
@@ -26,3 +26,31 @@ export class Food {
     }
 }
 
+export class FoodNutritional {
+    id: number;
+    name: string;
+    kind: string;
+    portion: string;
+    unity: string;
+    net_weight: number;
+    calories: number;
+    protein: number;
+    lipids: number;
+    cho: number;
+
+    constructor(_id: number, _name: string, _kind: string, _portion: string, _unity: string,
+    _net_weight: number, _calories: number, _protein: number, _lipids: number, _cho: number
+    ) {
+        this.id = _id;
+        this.name = _name;
+        this.kind = _kind;
+        this.portion = _portion;
+        this.unity = _unity;
+        this.net_weight = _net_weight;
+        this.calories = _calories;
+        this.protein = _protein;
+        this.lipids = _lipids;
+        this.cho = _cho;
+    }
+}
+
--- src/app/modules/nutritional-information/nutritional-information.component.ts
+++ src/app/modules/nutritional-information/nutritional-information.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Food } from '../../classes/Food';
+import { Food, FoodNutritional } from '../../classes/Food';
 import { FoodPreferenceAnswer } from '../../classes/FoodPreference';
 import { FoodService } from '../../services/food.service';
 
@@ -13,7 +13,7 @@ export class NutritionalInformationComponent implements OnInit {
   public FoodInfo: Food; // En este se carga el item food que se mostrara
   public AllFoods: Food[] = [];
   public UserFoodsAnwer: FoodPreferenceAnswer[];
-  public UserFoods: Food[] = []; // Son los foods del usuario
+  public UserFoods: FoodNutritional[] = []; // Son los foods del usuario
 
   constructor(
     public foodService: FoodService
@@ -30,10 +30,85 @@ export class NutritionalInformationComponent implements OnInit {
             for (let itemUser = 0; itemUser < this.UserFoodsAnwer.length; itemUser++) {
               if (this.AllFoods[item].id === this.UserFoodsAnwer[itemUser].id) {
                 console.log('Holi');
-                this.UserFoods.push(new Food(
+                let kindString;
+                switch (this.AllFoods[item].kind) {
+                  case 1: {
+                    kindString = 'Frutas';
+                    break;
+                  }
+                  case 2: {
+                    kindString = 'Verduras';
+                    break;
+                  }
+                  case 3: {
+                    kindString = 'Cereales sin grasa';
+                    break;
+                  }
+                  case 4: {
+                    kindString = 'Cereales con grasa';
+                    break;
+                  }
+                  case 5: {
+                    kindString = 'Leguminosas';
+                    break;
+                  }
+                  case 6: {
+                    kindString = 'A.O.A muy bajos en grasa';
+                    break;
+                  }
+                  case 7: {
+                    kindString = 'A.O.A bajos en grasa';
+                    break;
+                  }
+                  case 8: {
+                    kindString = 'A.O.A moderado en grasa';
+                    break;
+                  }
+                  case 9: {
+                    kindString = 'A.O.A alto en grasa';
+                    break;
+                  }
+                  case 10: {
+                    kindString = 'Leche descremada';
+                    break;
+                  }
+                  case 11: {
+                    kindString = 'Leche semidescremada';
+                    break;
+                  }
+                  case 12: {
+                    kindString = 'Leche entera';
+                    break;
+                  }
+                  case 13: {
+                    kindString = 'Leche con azucar';
+                    break;
+                  }
+                  case 14: {
+                    kindString = 'Aceites y grasas sin proteina';
+                    break;
+                  }
+                  case 15: {
+                    kindString = 'Aceites y grasas con proteina';
+                    break;
+                  }
+                  case 16: {
+                    kindString = 'Azucares sin grasa';
+                    break;
+                  }
+                  case 17: {
+                    kindString = 'Azucares con grasa';
+                    break;
+                  }
+                  case 18: {
+                    kindString = 'Alimentos libres de energia';
+                    break;
+                  }
+                }
+                this.UserFoods.push(new FoodNutritional(
                   this.AllFoods[item].id,
                   this.AllFoods[item].name,
-                  this.AllFoods[item].kind,
+                  kindString,
                   this.AllFoods[item].portion,
                   this.AllFoods[item].unity,
                   this.AllFoods[item].net_weight,
