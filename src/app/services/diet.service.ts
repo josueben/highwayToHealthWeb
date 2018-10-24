34bd3bcba84adb362099a0a4589a21eff1ae8783
josueben <josuebenbeatle@outlook.com>
2018-10-24 01:24:13 -0500

    dsarsaeqwrewq

--- src/app/services/diet.service.ts
+++ src/app/services/diet.service.ts
@@ -7,7 +7,7 @@ import { DietPost, Diet } from '../classes/Diet';
 })
 export class DietService {
 
-  url = 'http://192.168.1.50:5000';
+  url = 'http://localhost:5000';
   constructor(
     private http: HttpClient
   ) { }
