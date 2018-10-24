56186c905b147a3d1256f52dbc4705495daffaa0
Daniel Zapata <Animex_004@hotmail.com>
2018-10-24 02:01:57 -0500

    Lasto

--- src/app/modules/tracing/tracing.component.ts
+++ src/app/modules/tracing/tracing.component.ts
@@ -10,32 +10,23 @@ import { UserService } from '../../services/user.service';
 })
 
 export class TracingComponent implements OnInit {
+
   tracings: Tracing[] = [];
+
+  // ==================== Chart ====================
+
+  // Peso
   public lineChartData:Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Peso'},
   ];
+
+  // Fecha
   public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
   public lineChartOptions:any = {
     responsive: true
   };
-  public lineChartColors:Array<any> = [
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    },
-    { // dark grey
-      backgroundColor: 'rgba(77,83,96,0.2)',
-      borderColor: 'rgba(77,83,96,1)',
-      pointBackgroundColor: 'rgba(77,83,96,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(77,83,96,1)'
-    },
-    { // grey
+  public lineChartColors = [
+    { // fala escala de verdes
       backgroundColor: 'rgba(148,159,177,0.2)',
       borderColor: 'rgba(148,159,177,1)',
       pointBackgroundColor: 'rgba(148,159,177,1)',
@@ -66,6 +57,8 @@ export class TracingComponent implements OnInit {
   public chartHovered(e:any):void {
     console.log(e);
   }
+
+  // ==================== /Chart ====================
 
   constructor(
     public userService: UserService
